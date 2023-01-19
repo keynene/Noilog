5910df746d1b2c84e845ba3dfd88ebeff45b388f
keynene <nonung2@naver.com>
2023-01-19 18:12:57 +0900

    Add Setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
+/* eslint-disable */ //warning 제거
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 
+/* 컴포넌트 import */
+import { Join } from './routes/Join.js'
+
 function App() {
+
+  let navigate = useNavigate();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">T-Board</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link onClick={()=>{ navigate('/') }}>Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        {/* 메인페이지 */}
+        <Route path="/" element={ <div>메인페이지임</div> } />
+
+        {/* 회원가입페이지 */}
+        <Route path="/" element={ <Join /> } />
+      </Routes>
+
     </div>
   );
 }
