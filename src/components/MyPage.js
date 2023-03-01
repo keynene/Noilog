460d3af7a39c1f619922868cc6b9654775953381
keynene <nonung2@naver.com>
2023-03-01 19:28:28 +0900

    마이페이지 구현 (이미지 엑박)

--- src/components/MyPage.js
+++ src/components/MyPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function MyPage({ userObj }){
+function MyPage(){
 	return(
 		<div>
 				<div>
-
+					<p>왜 안나오노</p>
+					<img src="sample.jpg" alt="" width="133px" height="158px" />
 				</div>
 		</div>
 	)
