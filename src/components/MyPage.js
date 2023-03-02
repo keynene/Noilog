b70de7dcb051626f164c83e758a2dc1fe1ad73b6
keynene <nonung2@naver.com>
2023-03-02 18:07:20 +0900

    마이페이지 구현 (회원탈퇴 기능 에러)

--- src/components/MyPage.js
+++ src/components/MyPage.js
@@ -6,13 +6,26 @@ import Col from 'react-bootstrap/Col';
 
 import sampleImgUrl from '../img/sample.jpg'
 
+import { useDispatch } from "react-redux";
+import { deleteUserObj } from 'store.js';
+
 function MyPage(){
+	// let state = useSelector((state) => {return state})
+	let userId = JSON.parse(localStorage.getItem('login')).loginId
+	let userInfo = JSON.parse(localStorage.getItem(JSON.stringify(userId)))
+	
+	let dispatch = useDispatch();
+
 	return(
 		<div>
-			<Container>
+			<Container style={{marginTop:30}}>
 				<Row>
 					<Col><img src={sampleImgUrl} alt="" width="133px" height="158px" /></Col>
-					<Col>2 of 2</Col>
+					<Col style={{paddingTop:20}}>
+						<h4>{userInfo.nickname} ({userInfo.id})</h4>
+						<p>{userInfo.email}</p>
+						<button onClick={dispatch(deleteUserObj(userId))} >회원탈퇴</button>
+					</Col>
 				</Row>
 			</Container>
 		</div>
