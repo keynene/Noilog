c99ba7be8c7c7533aaff213977582729fec11690
keynene <nonung2@naver.com>
2023-03-08 18:51:20 +0900

    피드 컴포넌트 분리 : 피드, 피드팩토리 (댓글 컴포넌트도 분리 예정)

--- src/components/FeedFactory.js
+++ src/components/FeedFactory.js
@@ -1,52 +1,107 @@
 import React, { useEffect, useState } from 'react';
 
-import {useSelector} from "react-redux";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { createFeedObj } from 'store.js';
 
 function FeedFactory(){
-	const [feed, setFeed] = useState("");
-	const [feeds, setFeeds] = useState([]);
-
-	let userId = JSON.parse(localStorage.getItem('login')).loginId
-	let userInfo = JSON.parse(localStorage.getItem(JSON.stringify(userId)))
+	
 	let state = useSelector((state) => state)
 	let dispatch = useDispatch();
+	
+	const [feedTitle, setFeedTitle] = useState("");
+	const [feedContent, setFeedContent] = useState("");
+	let [postNumber, setPostNumber] = useState(0);
 
 	useEffect(()=>{
-		setFeeds(state.feedObj)
-	},[])
+		setPostNumber(postNumber)
+	},[postNumber])
 
-	const onChange = (e) => {
+	const onFeedTitleChange = (e) => {
 		const {
 			target: { value },
 		} = e;
-		setFeed(value);
+		setFeedTitle(value);
+	}
+
+	const onFeedContentChange = (e) => {
+		const {
+			target: { value },
+		} = e;
+		setFeedContent(value);
+	}
+
+	const getDate = () => {
+		let date = new Date();
+		let year = date.getFullYear();
+		let month = ("0" + (1+date.getMonth())).slice(-2);
+		let day = ("0"+date.getDate()).slice(-2);
+
+		return year + "-" + month + "-" + day;
+	}
+
+	const timeForToday = () => {
+		const today = new Date();
+		const timeValue = new Date(Date.now());
+
+		const betweenTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60);
+		if (betweenTime < 1) return '방금전';
+		if (betweenTime < 60) {
+				return `${betweenTime}분전`;
+		}
+
+		const betweenTimeHour = Math.floor(betweenTime / 60);
+		if (betweenTimeHour < 24) {
+				return `${betweenTimeHour}시간전`;
+		}
+
+		const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+		if (betweenTimeDay < 365) {
+				return `${betweenTimeDay}일전`;
+		}
+
+		return `${Math.floor(betweenTimeDay / 365)}년전`;
 	}
 
 	const onSubmit = (e) => {
 		e.preventDefault();
-		if (feed === ""){
-			return alert('피드를 입력해주세요')
-		}
-		let feedFactoryObj = [{
-			text : feed,
-			createAt : Date.now(),
-			creatorId : userId,
-			creatorNickname : userInfo.nickname,
-		}]
 
-		// dispatch(createFeedObj(feedFactoryObj))
-		let copy = [...feedFactoryObj.text, ...feeds]
-		setFeeds(copy)
-		feedFactoryObj = []
+		if (feedTitle === ""){
+			return alert('제목을 입력해주세요')
+		} 
+		
+		else if (feedContent === ""){
+			return alert('내용을 입력해주세요')
+		}
+
+		setPostNumber(postNumber+1)
+		console.log(state.userInfo)
+
+		let createdFeedObj = {
+			postNumber,
+			title : feedTitle,
+			content : feedContent,
+			writer : state.userId,
+			viewCount : 0,
+			likeCount : 0,
+			commentCount : 0,
+			createDate : timeForToday(),
+			creatorNickname : state.userInfo.nickname,
+		}
+		
+		dispatch(createFeedObj(createdFeedObj));
+
+		setFeedTitle("")
+		setFeedContent("")
+		
+		createdFeedObj = null
 	}
 
-
-	return (
+	return(
 		<div>
+			<h4 style={{marginBottom:30, marginTop:30}} >Feed</h4>	
 			<form onSubmit={onSubmit}>
-				<input type="text" placeholder="여기입력해봐" onChange={onChange} value={feed} />
+				<p><input type="text" name="title" placeholder="Title" onChange={onFeedTitleChange} value={feedTitle} /></p>
+				<p><input type="text" name="content" placeholder="Content" onChange={onFeedContentChange} value={feedContent} /></p>
 				<input type="submit" value="글작성하기" />
 			</form>
 		</div>
