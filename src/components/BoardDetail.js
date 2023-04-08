18bb6121432b6382f6ba352fe7ecb3009d06bdac
keynene <nonung2@naver.com>
2023-04-08 10:27:43 +0000

    게시글 상세페이지 함수 분리

--- src/components/BoardDetail.js
+++ src/components/BoardDetail.js
@@ -11,7 +11,7 @@ import { useNavigate } from 'react-router-dom';
 
 /* Redux, State */
 import { useDispatch, useSelector } from 'react-redux';
-import { onBoardLikeCountChange, deleteBoardObj, boardEditingOn, boardEditingOff } from 'store';
+import { onBoardLikeCountChange, deleteBoardObj, boardEditingOn, boardEditingOff, setOpenBoard, increaseBoardViewCount } from 'store';
 
 /* Components */
 import BoardCommentFactory from './BoardCommentFactory';
@@ -67,6 +67,63 @@ function BoardDetail(){
 		return data
 	}
 
+  const onEditButtonClick = () => {
+    if (window.confirm('게시글을 수정하시겠습니까?')){
+      dispatch(boardEditingOn(boards.boardNumber))
+    }
+  }
+  
+  const onDeleteButtonClick = () => {
+    if (window.confirm('게시글을 삭제하시겠습니까?')){
+      dispatch(deleteBoardObj(boards.boardNumber))
+      alert('삭제되었습니다.')
+      navigate("/")
+    }
+    
+  }
+
+  const onNextButtonClick = () => {
+    if(boardsObj.length === boards.boardNumber+1){
+      return alert('다음글이 없습니다!')
+    } else{
+      let nextId = state.userInfo.id
+      let nextBoardNumber = boards.boardNumber+1
+      while (nextBoardNumber < boardsObj.length){
+        let index = state.boardObj.findIndex((x)=> x.boardNumber === nextBoardNumber )
+        if (state.boardObj[index].content === ""){
+          nextBoardNumber++;
+        } else{
+          dispatch(setOpenBoard(nextBoardNumber))
+          dispatch(increaseBoardViewCount({nextId, nextBoardNumber}))
+          navigate("/boarddetail")
+          break;
+        }
+      }
+      return alert('다음글이 없습니다!')
+    }
+  }
+
+  const onPrevButtonClick = () => {
+    if(boards.boardNumber === 0){
+      return alert('이전글이 없습니다!')
+    } else{
+      let prevId = state.userInfo.id
+      let prevBoardNumber = boards.boardNumber-1
+      while (prevBoardNumber >= 0){
+        let index = state.boardObj.findIndex((x)=> x.boardNumber === prevBoardNumber )
+        if (state.boardObj[index].content === ""){
+          prevBoardNumber--;
+        } else{
+          dispatch(setOpenBoard(prevBoardNumber))
+          dispatch(increaseBoardViewCount({prevId, prevBoardNumber}))
+          navigate("/boarddetail")
+          break;
+        }
+      }
+      return alert('이전글이 없습니다!')
+    }
+  }
+
 	return (
     <Container style={{width:800, marginTop:10, marginBottom:100}}>
       <Row style={{textAlign:'left'}}>
@@ -93,6 +150,7 @@ function BoardDetail(){
           <span>추천 : {boards.likeCount.length}</span>
         </Col>
       </Row>
+
       {/* 수정폼 컴포넌트 */}
       { state.isBoardEditing.editState ? (
         <BoardEditForm boards={boards} />
@@ -102,48 +160,58 @@ function BoardDetail(){
         <Row style={{marginTop:15}}>
           <Col style={{textAlign:'right'}}>
             {/* 수삭목댓 컴포넌트 */}
-            <BoardUpDelInCom isBoardOwner={isBoardOwner} boards={boards} navigate={navigate} dispatch={dispatch} />
+            <BoardUpDelInCom isBoardOwner={isBoardOwner} boards={boards} navigate={navigate} onEditButtonClick={onEditButtonClick} onDeleteButtonClick={onDeleteButtonClick}/>
           </Col>
         </Row>
+        
         <Row style={{marginTop:40, marginBottom:60}}>
           <Col><h4>{boards.title}</h4></Col>
         </Row>
+
         <Row>
           <Col style={{textAlign:'left', paddingBottom:80}} >
             <div dangerouslySetInnerHTML={{ __html :  boards.content  }} />
           </Col>
         </Row>
+
+        {/* 추천버튼 컴포넌트 */}
         <Row>
           <Col style={{alignItems:'baseline'}}>
-            {/* 추천버튼 컴포넌트 */}
             <LikeButton state={state} boards={boards} dispatch={dispatch} navigate={navigate} dataObj={dataObj} />
           </Col>
         </Row>
+
         <Row style={{marginTop:30, alignItems:'flex-end', paddingBottom:30, borderBottom:'1px solid #ccc'}}>
           <Col style={{textAlign:'left'}}>
             {/* 수삭목댓 컴포넌트 */}
-            <BoardUpDelInCom isBoardOwner={isBoardOwner} boards={boards} navigate={navigate} dispatch={dispatch} />
+            <BoardUpDelInCom isBoardOwner={isBoardOwner} boards={boards} navigate={navigate} onEditButtonClick={onEditButtonClick} onDeleteButtonClick={onDeleteButtonClick}/>
           </Col>
           <Col style={{textAlign:'right'}}>
             { isBoardOwner ? (
+              <>
               <Button 
                 variant="light" 
                 style={{marginRight:10, border:'1px solid rgb(200,200,200)'}} 
-                onClick={()=>{
-                  if (window.confirm('게시글을 수정하시겠습니까?')){
-                    dispatch(boardEditingOn(boards.boardNumber))
-                  }
-              }}>수정</Button> ) : null 
+                onClick={()=>{onEditButtonClick()}}
+              >수정</Button> 
+              <Button
+                variant="light"
+                style={{marginRight:10, border:'1px solid rgb(200,200,200)'}}
+                onClick={()=>{onDeleteButtonClick()
+                }}
+              >삭제</Button>
+              </>
+              ) : null 
             }
             <Button variant="light" style={{marginRight:10, border:'1px solid rgb(200,200,200)'}} onClick={()=>{navigate("/")}}>목록</Button>
             <BoardWriteButton />
           </Col>
         </Row>
+
+        {/* 댓글출력 컴포넌트 */}
         <Row style={{marginTop:10}}>
           <Col style={{textAlign:'left'}}><BiCommentDetail/> 댓글 ({boards.commentCount})</Col>
         </Row>
-
-        {/* 댓글출력 컴포넌트 */}
         <Row>
           <Col>
             <Container>
@@ -165,41 +233,41 @@ function BoardDetail(){
         {/* 댓글달기 컴포넌트 */}
         <BoardCommentFactory boards={boards} />
 
-        <Row style={{textAlign:'left', marginTop:30}}>
+        {/* 목록, 다음글, 이전글, 맨위로, 글쓰기 버튼 */}
+        <Row style={{textAlign:'left' , marginTop:30}}>
           <Col>
             <Button variant="light" onClick={()=>{navigate("/")}} style={{border:'1px solid rgb(200,200,200)', marginRight:5}}>목록</Button>
-            {/* <Button variant="light" onClick={()=>{navigate("/")}} style={{fontSize:13, border:'1px solid rgb(200,200,200)', marginRight:5}}>다음글 ↑</Button>
-            <Button variant="light" onClick={()=>{navigate("/")}} style={{fontSize:13, border:'1px solid rgb(200,200,200)'}}>이전글 ↓</Button> */}
+            <Button 
+              variant="light" 
+              onClick={()=>{onNextButtonClick()}}
+              style={{border:'1px solid rgb(200,200,200)', marginRight:5}}
+            >다음글 ↑</Button>
+            <Button 
+              variant="light" 
+              onClick={()=>{onPrevButtonClick()}} 
+              style={{border:'1px solid rgb(200,200,200)'}}
+            >이전글 ↓</Button>
           </Col>
           <Col style={{textAlign:'right'}}>
             <Button variant="light" onClick={()=>{MoveToTop()}} style={{border:'1px solid rgb(200,200,200)', marginRight:5}}>맨위로</Button>
             <BoardWriteButton />
           </Col>
         </Row>
-      </>
+      </> //수정중이 아닐 때 게시글 출력
       )}
     </Container>
 	)
 }
 
 /* 수정/삭제/목록/댓글 span */
-function BoardUpDelInCom({ isBoardOwner, boards, navigate, dispatch }){
+function BoardUpDelInCom({ isBoardOwner, boards, navigate, onEditButtonClick, onDeleteButtonClick }){
   return (
     <div>
       { isBoardOwner ? (
         // 글작성자이면 수삭목댓
         <div style={{fontSize:14, color:'gray'}}>
-          <span style={{cursor:'pointer'}} onClick={()=>{
-            if (window.confirm('게시글을 수정하시겠습니까?')){
-              dispatch(boardEditingOn(boards.boardNumber))
-            }
-          }}>수정</span><span> | </span>
-          <span style={{cursor:'pointer'}} onClick={()=>{
-            if (window.confirm('게시글을 삭제하시겠습니까?')){
-              dispatch(deleteBoardObj(boards.boardNumber))
-              navigate("/")
-            }}} >삭제
-          </span><span> | </span>
+          <span style={{cursor:'pointer'}} onClick={()=>{ onEditButtonClick() }}>수정</span><span> | </span>
+          <span style={{cursor:'pointer'}} onClick={()=>{ onDeleteButtonClick() }}>삭제</span><span> | </span>
           <span style={{cursor:'pointer'}} onClick={()=>{navigate("/")}}>목록</span><span> | </span>
           <span style={{cursor:'pointer'}}>댓글(<span style={{color:'#F94B4B'}}>{boards.commentCount}</span>)</span>
         </div>
