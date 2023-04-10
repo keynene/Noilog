dd1c4ebc49fe53e2696844199eeae968ed299127
keynene <nonung2@naver.com>
2023-04-10 10:42:17 +0000

    페이지네이션 라이브러리 다운로드, 테스트파일 생성 : react-js-pagination, : test.js 생성

---
+++ src/components/test.js
@@ -0,0 +1,392 @@
+let data = [{
+	"postNumber": 30,
+	"title": "23 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:58:31.78719"
+},
+{
+	"postNumber": 29,
+	"title": "22 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:58:29.99624"
+},
+{
+	"postNumber": 28,
+	"title": "21 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:58:28.095811"
+},
+{
+	"postNumber": 27,
+	"title": "20 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:58:26.032758"
+},
+{
+	"postNumber": 26,
+	"title": "19 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:58:23.680813"
+},
+{
+	"postNumber": 25,
+	"title": "18 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:58:21.313921"
+},
+{
+	"postNumber": 24,
+	"title": "17 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:58:19.667398"
+},
+{
+	"postNumber": 23,
+	"title": "16 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:58:17.735696"
+},
+{
+	"postNumber": 22,
+	"title": "15 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:58:15.671409"
+},
+{
+	"postNumber": 21,
+	"title": "14 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:58:14.361511"
+},
+{
+	"postNumber": 20,
+	"title": "13 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:58:12.704394"
+},
+{
+	"postNumber": 19,
+	"title": "12 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:58:11.409712"
+},
+{
+	"postNumber": 18,
+	"title": "11 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:58:07.818529"
+},
+{
+	"postNumber": 17,
+	"title": "10 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:58:05.51697"
+},
+{
+	"postNumber": 16,
+	"title": "9 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:58:01.535189"
+},
+{
+	"postNumber": 15,
+	"title": "8 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:57:59.720808"
+},
+{
+	"postNumber": 14,
+	"title": "7 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:57:57.524282"
+},
+{
+	"postNumber": 13,
+	"title": "6 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:57:54.806459"
+},
+{
+	"postNumber": 12,
+	"title": "5 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:57:52.342949"
+},
+{
+	"postNumber": 11,
+	"title": "4 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:57:50.722463"
+},
+{
+	"postNumber": 10,
+	"title": "3 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:57:49.283588"
+},
+{
+	"postNumber": 9,
+	"title": "2 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:57:47.835875"
+},
+{
+	"postNumber": 8,
+	"title": "1 title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-04-12T16:57:44.531147"
+},
+{
+	"postNumber": 7,
+	"title": "xx title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-03-17T17:06:02.91354"
+},
+{
+	"postNumber": 6,
+	"title": "xx title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-03-17T17:05:33.195744"
+},
+{
+	"postNumber": 5,
+	"title": "xx title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-03-17T17:03:31.11482"
+},
+{
+	"postNumber": 4,
+	"title": "xx title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-03-17T17:03:16.459097"
+},
+{
+	"postNumber": 3,
+	"title": "xx title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-03-17T17:01:55.053652"
+},
+{
+	"postNumber": 2,
+	"title": "xx title",
+	"content": "zz content",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-03-17T17:01:29.920622"
+},
+{
+	"postNumber": 1,
+	"title": "admin",
+	"content": "admin",
+	"writer": {
+			"memberNumber": 1,
+			"nickname": "nick-admin"
+	},
+	"viewCount": 0,
+	"likeCount": 0,
+	"commentCount": 0,
+	"createdDate": "2023-03-17T17:01:29.920622"
+}]
+
+export default data;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,23 +1,80 @@
 import React, { useEffect, useState } from 'react';
 
-import { Table } from 'react-bootstrap';
+import { Button, Table } from 'react-bootstrap';
 
 import { useSelector } from 'react-redux';
 
 /* Components */
 import BoardWriteButton from 'components/BoardWriteButton';
 import BoardRow from 'components/BoardRow';
+import data from 'components/test.js'
+
+// currentPage: 현재 페이지
+// totalCount: 총 데이터의 갯수
+// pageCount: 화면에 나타날 페이지 갯수
+// limit: 한 페이지 당 나타낼 데이터의 갯수
 
 function Home(){
 	let state = useSelector((state) => state)
 
 	let [boards, setBoards] = useState([]);
 
+
+
+	////////////////////////////////////////////////////////////////////////////////////
+	let [tBoards, setTBoards] = useState(data);
+
+	const [page, setPage] = useState(1); //페이지
+	const [totalPosts, setTotalPosts] = useState(tBoards.length); //게시글 갯수
+	const limit = 10; //posts가 보일 최대한의 갯수
+	const offset = (page-1)*limit //시작점과 끝점 구하기
+	const numPages = Math.ceil(totalPosts/limit)
+
+	const postsData = (posts) => {
+		if(posts){
+			let result = posts.slice(offset, offset+limit);
+			return result;
+		}
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////
+
 	useEffect(()=>{
 		setBoards([...state.boardObj].reverse())
 	},[state.boardObj])
 
 	return (
+		// 원본
+		// <div style={{maxWidth:800, marginLeft:'auto', marginRight:'auto'}}>
+		// 	<h4 style={{marginTop:30}} >Board</h4>	
+		// 	<div style={{textAlign:'right'}}>
+		// 		<BoardWriteButton />
+		// 	</div>
+		// 	<Table style={{marginTop:30, width:800}}>
+		// 		<thead>
+		// 			<tr style={{width:800}}>
+		// 				<th style={{width:80}}>번호</th>
+		// 				<th style={{width:400}}>제목</th>
+		// 				<th style={{width:100}}>작성자</th>
+		// 				<th style={{width:100}}>작성일시</th>
+		// 				<th style={{width:60}}>조회수</th>
+		// 				<th style={{width:60}}>추천</th>
+		// 			</tr>
+		// 		</thead>
+		// 		<tbody>
+		// 			{ boards.map((a,i) => 
+		// 				boards[i].content !== "" ?
+		// 					<BoardRow boards={boards} key={i} i={i} />
+		// 				: null
+		// 			)}
+		// 		</tbody>
+		// 	</Table>
+		// </div>
+
+
+
+
+		/////////////////////////////////////////////////////////////////////////사본
 		<div style={{maxWidth:800, marginLeft:'auto', marginRight:'auto'}}>
 			<h4 style={{marginTop:30}} >Board</h4>	
 			<div style={{textAlign:'right'}}>
@@ -35,14 +92,29 @@ function Home(){
 					</tr>
 				</thead>
 				<tbody>
-					{ boards.map((a,i) => 
-						boards[i].content !== "" ?
-							<BoardRow boards={boards} key={i} i={i} />
-						: null
+					{ tBoards.map((a,i) => 
+						<tr key={i} style={{fontSize:15}}>
+							<td>{tBoards[i].postNumber}</td>
+							<td>{tBoards[i].title}</td>
+							<td>{tBoards[i].writer.nickname}</td>
+							<td>{tBoards[i].createdDate.substring(0,10)}</td>
+							<td>{tBoards[i].viewCount}</td>
+							<td>{tBoards[i].likeCount}</td>
+						</tr>
 					)}
 				</tbody>
 			</Table>
+			<div>
+				{Array(numPages).map((pa,pi) => 
+					<Button 
+						variant="light"
+						key={pi+1}
+						onClick={()=>{setPage(pi+1)}}
+					>{pi+1}</Button>
+				)}
+			</div>
 		</div>
+		/////////////////////////////////////////////////////////////////////////사본
 	)
 }
 
